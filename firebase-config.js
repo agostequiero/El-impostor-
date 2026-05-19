95fac23672e73d8375d923cb2e20a9b9b7bc135a
agostequiero <wa.wa.wa.wa.chukumen@gmail.com>
2026-05-19 02:24:31 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyB8eRbkeyE0w0wLl10QVSa0JDKU_t49Hko",
+  authDomain: "el-impostorr.firebaseapp.com",
+  databaseURL: "https://el-impostorr-default-rtdb.firebaseio.com",
+  projectId: "el-impostorr",
+  storageBucket: "el-impostorr.firebasestorage.app",
+  messagingSenderId: "949215994691",
+  appId: "1:949215994691:web:a9b41638ad01f4142345f6"
 };
 
 export function hasFirebaseConfig() {
